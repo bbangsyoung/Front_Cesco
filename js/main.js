29e2d26f626f8e73c5f575715e6fe18db9117847
syoung <dividividid@gmail.com>
2022-09-05 21:03:33 +0900

    index 전반적인 기능 수정
    1. 네비게이션 수정 - 슬라이드 형식으로 아예 수정 및 완성
    2. 컨텐츠 정리 및 이미지 hover 추가
    3. 최하단 이벤트 배너 - 동적 제이쿼리 추가
    
    중간에 8개짜리 배너 부분 제이쿼리 마우스아웃 시 동작이 안되는걸 확인. 내일 강사님께 여쭤볼 예정.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,97 @@
+function banner1() {
+  $(".food").animate({
+      marginLeft : parseInt($(".food").css("margin-left"))-240+"px"},"slow",function(){
+          $(".food li:first").appendTo(".food");
+          $(".food").css("margin-left","-240px");
+      });
+}
+
 $(document).ready(function(){
 
-//네비게이션
-$(".main_sub_navi").slideUp(1000);
-$("#main_navi_top").bind("mouseenter", function () {
-  $(".main_sub_navi").stop().slideDown("fast", "easeInOutCubic");
+//맨위긴메뉴 - 왜안되는겅미
+$(".container").sliphover({
 });
-$(".main_sub_menu").bind("mouseleave", function () {
-  if($("#main_navi_top").is(":focus")==true) {
-      $(".main_sub_menu").show();
-  } else {
-      $(".main_sub_menu").stop().slideUp(800,"easeInOutCubic");
-  }
-}).focusout(function(){
-  $("#main_navi_top").stop().slideUp(200, "easeInOutCubic");
+
+
+
+//배너8개짜리 호버
+$(".b_e span").text("");
+$(".b_e").mouseenter(function(){  
+    $(".b_e span:eq(0)").animate({width: "94%"},100,function(){
+        $(this).next().animate({height: "93%"},100,function(){
+          $(".b_e span:eq(1)").css("opacity","1");
+            $(this).next().animate({width: "94%"},100,function(){
+              $(".b_e span:eq(3)").css("opacity","1");
+                $(this).next().animate({height: "93%"},300);
+            });
+        });
+    });
+}).mouseleave(function(){
+  $(".b_e span:eq(0)").stop().css("width","0px","height","2px");
+  $(".b_e span:eq(1)").stop().css("opacity","0");
+  $(".b_e span:eq(2)").stop().css("width","0px","height","2px");
+  $(".b_e span:eq(3)").stop().css("opacity","0");
+  
+})
+
+
+  //아래배너 연습
+var width1 = 240*$(".food li").size() +"px";
+    $(".food").css("width",width1); //240x16 = 3840
+    $(".food li:last").prependTo(".food");
+    $(".food").css("margin-left","-240px");
+
+    var banner = setInterval("banner1()",1500);
+
+    $(".food li").mouseover(function(){
+        clearInterval(banner);
+        $(this).find("img").css("opacity","0.5")
+    }).mouseout(function(){
+        banner = setInterval("banner1()",1500);
+        $(this).find("img").css("opacity","1")
+    })
+
+
+
+
+
+
+
+
+
+
+//태그추가\
+$("#main_navi").before("<div class='bg'></div>")
+
+$(".bg, .sub").hide();
+$("#menu").mouseenter(function(){
+    $(".bg,.sub").stop().slideDown("fast");
+   $("#menu>li>a").delay(3000).css("color","#666");  
+}).mouseleave(function(){
+    $(".bg,.sub").stop().slideUp("fast");
+    $("#menu>li>a").delay(3000).css("color","");
+})
+
+$("#menu>li>a").mouseover(function(){
+  $(this).css("color","#00b4ff");
+}).mouseout(function(){
+   $("#menu>li>a").css("color","#666");
+})
+
+
+//마우스 올려져있을때 고정시켜놓을 bg영역
+$(".bg").mouseover(function(){
+    $(this).stop().show();
+    $(".sub").stop().show();
+}).mouseover(function(){
+    $(this).stop().slideUp();
+    $(".sub").stop().slideUp();
 })
 
 
 
+
+//네비게이션
 
 
   $(function() {
@@ -72,21 +147,9 @@ $(".con3_top_img").mouseover(function(){
 })
 
 
+   
+    
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  });
+  });//end
